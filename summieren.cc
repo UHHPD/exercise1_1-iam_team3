1c667d7f220185871707d9ffb57520c51c805b1d
BentjeM <116177631+BentjeM@users.noreply.github.com>
2022-11-02 17:41:38 +0100

    Update summieren.cc

--- summieren.cc
+++ summieren.cc
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <iostream>
 #include <fstream>
 using namespace std;
@@ -21,37 +20,5 @@ int main(){
    }
   data_in.close();
   data_out.close();
+  return 0;
 }
-=======
-#include<iostream>
-#include<fstream>
-using namespace std;
-
-int main(){
-   
-  ifstream fin("daten.txt");
-  ofstream fout("datensumme.txt");
-  int a=0;
-  int b=0;
-
-   
-  while(!fin.eof())
-    { fin>>a;
-      fin>>b;   
-    
-      if (fin.eof()) {
-       break;
-      }
-      cout<<a+b<<endl;
-      fout<<a+b<<endl;
-      }
-  
-  fin.close();
-  fout.close();
-  
-}
-
-
-
-
->>>>>>> origin/master
